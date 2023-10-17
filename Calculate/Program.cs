45922fe78d5610913a5503f1651e3ef2089e191d
Sergey <klapischeff@yandex.ru>
2023-10-17 19:34:36 +0300

    Добавьте файлы проекта.

---
+++ Calculate/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Calculate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            string operation;
+
+            Console.Write("Введите a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.Write("Введите операцию(+ - * /): ");
+            operation = Console.ReadLine();
+
+            Console.Write("Введите b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            if(operation == "+")
+            {
+                Console.WriteLine($"{a} + {b} = { sum(a, b) }");
+            }
+            if (operation == "-")
+            {
+                Console.WriteLine($"{a} - {b} = {diff(a, b)}");
+            }
+            if (operation == "*")
+            {
+                Console.WriteLine($"{a} * {b} = {mult(a, b)}");
+            }
+            if (operation == "/")
+            {
+                Console.WriteLine($"{a} / {b} = {div(a, b)}");
+            }
+        }
+
+
+        public static int sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int diff(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int mult(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
